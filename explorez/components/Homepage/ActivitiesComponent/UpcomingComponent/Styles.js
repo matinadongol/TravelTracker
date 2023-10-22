6052183bdb362880707eac2c43d272fc3aeab677
matina <dongolmt@gmail.com>
2023-10-22 15:42:07 -0400

    added trip details shortcuts

---
+++ explorez/components/Homepage/ActivitiesComponent/UpcomingComponent/Styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop:20,
+        paddingBottom:25,
+    },
+    upcomingTxt:{
+        fontFamily:'bai',
+        fontSize:25,
+        color:'black',
+        marginBottom: 15
+    },
+    upcomingList:{
+        display: 'flex',
+        alignItems: 'stretch',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        marginBottom: 25,
+        borderColor: '#d8f5f4',  
+        borderWidth: 5, 
+        borderRadius: 10,
+        padding: 20
+    },
+    upcomingListDestination:{
+        fontSize: '25',
+        fontWeight: '500',
+        marginBottom: 3
+    }
+})
+export default styles;
--- explorez/components/Homepage/ActivitiesComponent/UpcomingComponent/UpcomingComponent.js
+++ explorez/components/Homepage/ActivitiesComponent/UpcomingComponent/UpcomingComponent.js
@@ -1,37 +1,51 @@
-import {View, Text, StyleSheet, ScrollView} from "react-native"
+import React, { useState, useEffect } from 'react';
+import {View, Text, ScrollView, FlatList} from "react-native"
 import { useFonts } from "expo-font"
+import * as database from '../../../database';
+import styles from './Styles';
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 
 const UpcomingComponent = () => {
-    let [fontLoaded] = useFonts({
-        'bai': require('../../../../assets/fonts/BaiJamjuree-Bold.ttf')
-    })
+    const [trips, setTrips] = useState([]);
+    const [fontLoaded] = useFonts({
+        'bai': require('../../../../assets/fonts/BaiJamjuree-Bold.ttf'),
+      });
 
-    if (!fontLoaded){
-        return null
-    } 
+      useEffect(() => {
+        if (fontLoaded) {
+          database.load().then((data) => {
+            setTrips(data);
+          });
+        }
+      }, [fontLoaded]);
+    
+      if (!fontLoaded) {
+        return null;
+      }
 
     return (
-        <View style={Styles.container}>
-            <Text style={Styles.upcomingTxt}>UPCOMING</Text>
+        <View style={styles.container}>
+            <Text style={styles.upcomingTxt}>UPCOMING</Text>
             <ScrollView>
-                
+                <FlatList
+                    data={trips}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                        <View style={styles.upcomingList}>
+                            <View style={styles.upcomingListDetails}>
+                                <Text style={styles.upcomingListDestination}>{item.destination}</Text>
+                                <Text style={styles.upcomingListStartDate}>{item.startDate}</Text>
+                            </View>
+                            <View style={styles.upcomingListButton}>
+                                <Icon name="angle-right" size={30} color="#000" />
+                            </View>
+                        </View>
+                    )}
+                    />
             </ScrollView>
         </View>
     )
 }
-
-const Styles = StyleSheet.create({
-    container: {
-        marginTop:20,
-        paddingBottom:25,
-    },
-    upcomingTxt:{
-        fontFamily:'bai',
-        fontSize:19,
-        color:'black',
-        marginBottom:5
-    }
-})
 
 export default UpcomingComponent
